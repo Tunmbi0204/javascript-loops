88da51ff5120251b57fb7c617e7f2005745233ce
TUNMBI <adelowotumbi1@gmail.com>
2022-10-17 00:28:59 +0200

    pushing my while loop

---
+++ while.js
@@ -0,0 +1,35 @@
+let output = document.getElementById('output2');
+let scores = [60, 77, 38, 19, 59, 47, 23];
+let text = "";
+
+
+
+let m = 0;
+while (m < scores.length)
+// while(m++);
+// for (let m = 0; m < scores.length; m++)
+{
+       m++;
+    let punishment = scores[m] - 5;
+    let manipulated = punishment + scores[m] / 2;
+    
+
+
+    console.log(punishment);
+
+ if (punishment > 50){
+  text += punishment + "- you passed wonderfully, your previous score is " + scores[m] + " after manipulation" + "<br>";
+     output.innerHTML = text;
+}
+
+ else if (manipulated > 50){
+text += manipulated + "- you escaped my punishment because of my manipulated score, your previous score is " + scores[m] + " after manipulation <br>";
+output.innerHTML = text;
+}
+
+ else {
+    text += punishment + "- you failed badly,your previous score " + scores[m] + " after manipulation <br> ";
+    output.innerHTML =  text;
+}
+
+}
